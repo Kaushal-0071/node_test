be0943d2116f862583d6aa418728a27a14f5e56c
Kaushal-0071 <kaushalpitlam@gmail.com>
2025-10-18 14:04:13 +0530

    clog

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const app = express()
 
 const PORT = 3001
 app.use(cors())
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) =>{
+    console.log('Hello World!')
+  res.send('Hello World!')
+})
 app.listen(PORT, () => console.log(`Test Server Running..${PORT}`))
