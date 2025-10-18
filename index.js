76615dbf2b20bae8862577caf0575c4fe28bb95c
Kaushal-0071 <kaushalpitlam@gmail.com>
2025-10-18 13:53:49 +0530

    cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
-
+const cors = require('cors')
 const app = express()
+
 const PORT = 3001
+app.use(cors())
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(PORT, () => console.log(`Test Server Running..${PORT}`))
